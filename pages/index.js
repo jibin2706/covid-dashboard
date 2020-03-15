cc5ffe2081c663c0ba0d08b22ff0d9cc65b586a2
Jibin Thomas <jibin.thomas@thesouledstore.com>
2020-03-15 21:41:59 +0530

    Animating number counter using react-countup

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,61 @@
 import Head from "next/head";
 import fetch from "node-fetch";
 import TimeseriesGraph from "../src/components/TimeseriesGraph";
+import { useCountUp } from "react-countup";
 
 import css from "../src/styles/index.module.scss";
 
-const Home = ({ global }) => (
-  <div className="container">
-    <Head>
-      <title>COVID-19</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const Home = ({ global }) => {
+  const { countUp: confirmedCount } = useCountUp({
+    end: global.confirmed,
+    duration: 1
+  });
+  const { countUp: deathCount } = useCountUp({
+    end: global.deaths,
+    duration: 1
+  });
+  const { countUp: recoveredCount } = useCountUp({
+    end: global.recovered,
+    duration: 1
+  });
 
-    <main>
-      <h1 className="title">COVID 19</h1>
-      <section className={css.cardContainer}>
-        <div className={css.card}>
-          <h2 className={css.cardHeader}>Confirmed</h2>
-          <h2 className={css.countNumber}>
-            {Number(global.confirmed).toLocaleString()}
-          </h2>
-        </div>
-        <div className={css.card}>
-          <h2 className={css.cardHeader}>Deaths</h2>
-          <h2 className={css.countNumber}>
-            {Number(global.deaths).toLocaleString()}
-          </h2>
-        </div>
-        <div className={css.card}>
-          <h2 className={css.cardHeader}>Recovered</h2>
-          <h2 className={css.countNumber}>
-            {Number(global.recovered).toLocaleString()}
-          </h2>
-        </div>
-      </section>
+  return (
+    <div className={css.container}>
+      <Head>
+        <title>COVID-19</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <section className={css.card} style={{ padding: 0, margin: "2rem 0" }}>
-        <TimeseriesGraph />
-      </section>
-    </main>
+      <main>
+        <h1 className={css.title}>COVID-19 </h1>
+        <section className={css.cardContainer}>
+          <div className={css.card}>
+            <h2 className={css.cardHeader}>Confirmed</h2>
+            <h2 className={css.countNumber}>
+              {Number(confirmedCount).toLocaleString()}
+            </h2>
+          </div>
+          <div className={css.card}>
+            <h2 className={css.cardHeader}>Deaths</h2>
+            <h2 className={css.countNumber}>
+              {Number(deathCount).toLocaleString()}
+            </h2>
+          </div>
+          <div className={css.card}>
+            <h2 className={css.cardHeader}>Recovered</h2>
+            <h2 className={css.countNumber}>
+              {Number(recoveredCount).toLocaleString()}
+            </h2>
+          </div>
+        </section>
 
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        max-width: 1000px;
-        margin: auto;
-        padding: 0 0.5rem;
-      }
-
-      main {
-        padding: 5rem 0;
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-);
+        {/* <section className={css.card} style={{ padding: 0, margin: "2rem 0" }}>
+          <TimeseriesGraph />
+        </section> */}
+      </main>
+    </div>
+  );
+};
 
 export async function getStaticProps() {
   const res = await fetch("https://covid-dashboard.now.sh/api/global");
