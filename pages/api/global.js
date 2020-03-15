e65a835d98784d29b8d26a2fef67100b53ae9d7e
Jibin Thomas <jibin.thomas@thesouledstore.com>
2020-03-15 12:43:22 +0530

    Create /api/global to get global covid data

---
+++ pages/api/global.js
@@ -0,0 +1,25 @@
+import fetch from "node-fetch";
+
+export default async (req, res) => {
+  let totalGlobalCases = { confirmed: 0, deaths: 0, recovered: 0 };
+
+  if (req.method === "GET") {
+    await fetch("https://covid19.mathdro.id/api/confirmed")
+      .then(response => {
+        return response.json();
+      })
+      .then(result => {
+        result.forEach(item => {
+          totalGlobalCases = {
+            confirmed: totalGlobalCases.confirmed + item.confirmed,
+            deaths: totalGlobalCases.deaths + item.deaths,
+            recovered: totalGlobalCases.recovered + item.recovered
+          };
+        });
+      });
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(totalGlobalCases));
+  }
+};
