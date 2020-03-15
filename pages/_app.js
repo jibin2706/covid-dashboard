342b58bb8b60e1358febf5ad33dec40d20412a35
Jibin Thomas <jibin.thomas@thesouledstore.com>
2020-03-15 23:48:34 +0530

    Added google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
+import { initGA, logPageView } from "../src/components/googleAnalytics";
 import "../src/styles/global.scss";
 
 function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
   return <Component {...pageProps} />;
 }
 
