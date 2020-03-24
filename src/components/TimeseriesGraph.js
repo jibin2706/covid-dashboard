cb7f507b1cfa70a91190a019097179d19a4cddb2
Jibin Thomas <jibin.thomas2706@gmail.com>
2020-03-24 22:52:27 +0530

    Added .prettierrc config

--- src/components/TimeseriesGraph.js
+++ src/components/TimeseriesGraph.js
@@ -3,12 +3,12 @@ import { Tooltip, AreaChart, Area, ResponsiveContainer, XAxis } from "recharts";
 import globalData from "../../public/data/time-series.json";
 
 let data = [];
-data = Object.keys(globalData.confirmed).map(item => {
+data = Object.keys(globalData.confirmed).map((item) => {
   return {
     name: item,
     confirmed: globalData.confirmed[item],
     deaths: globalData.deaths[item],
-    recovered: globalData.recovered[item]
+    recovered: globalData.recovered[item],
   };
 });
 
@@ -17,22 +17,8 @@ function TimeseriesGraph() {
     <ResponsiveContainer>
       <AreaChart data={data} width={400} height={300}>
         <XAxis name="Days since 22Feb" dataKey="name"></XAxis>
-        <Area
-          stackId="1"
-          type="linear"
-          dataKey="deaths"
-          stroke="#dc3545"
-          fill="#dc3545"
-          name="Deaths"
-        />
-        <Area
-          stackId="1"
-          type="monotone"
-          dataKey="recovered"
-          stroke="#82ca9d"
-          fill="#82ca9d"
-          name="Recovered"
-        />
+        <Area stackId="1" type="linear" dataKey="deaths" stroke="#dc3545" fill="#dc3545" name="Deaths" />
+        <Area stackId="1" type="monotone" dataKey="recovered" stroke="#82ca9d" fill="#82ca9d" name="Recovered" />
         <Area
           stackId="1"
           type="monotone"
