4c38bfb31da071e48c50eeae3559b8478ddb8153
Jibin Thomas <jibin.thomas@thesouledstore.com>
2020-03-24 23:17:07 +0530

    Added next-offline plugin

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,7 @@ import dynamic from "next/dynamic";
 import fetch from "node-fetch";
 import { useCountUp } from "react-countup";
 
-const TimeseriesGraphClient = dynamic(
-  import("../src/components/TimeseriesGraph")
-);
+const TimeseriesGraphClient = dynamic(import("../src/components/TimeseriesGraph"));
 
 import css from "../src/styles/index.module.scss";
 
@@ -20,6 +18,21 @@ const Home = ({ global, countries }) => {
     }
   });
   const [error, setError] = React.useState("");
+
+  React.useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function() {
+        navigator.serviceWorker
+          .register("/service-worker.js", { scope: "/" })
+          .then(function(registration) {
+            console.log("SW registered: ", registration);
+          })
+          .catch(function(registrationError) {
+            console.log("SW registration failed: ", registrationError);
+          });
+      });
+    }
+  }, []);
 
   React.useEffect(() => {
     fetch(`https://covid19.mathdro.id/api/countries/${country}`)
@@ -67,17 +80,9 @@ const Home = ({ global, countries }) => {
         <title>COVID-19</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#222222" />
-        <meta
-          name="description"
-          content="COVID-19 dashboard with stats about global and each country outbreak"
-        />
+        <meta name="description" content="COVID-19 dashboard with stats about global and each country outbreak" />
         <meta name="application-name" content="COVID-19" />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
 
@@ -88,33 +93,22 @@ const Home = ({ global, countries }) => {
         <section className={css.cardContainer}>
           <div className={css.card} style={{ color: "#ffc107" }}>
             <h2 className={css.cardHeader}>Confirmed</h2>
-            <h2 className={css.countNumber}>
-              {Number(confirmedCount).toLocaleString()}
-            </h2>
+            <h2 className={css.countNumber}>{Number(confirmedCount).toLocaleString()}</h2>
           </div>
           <div className={css.card} style={{ color: "#dc3545" }}>
             <h2 className={css.cardHeader}>Deaths</h2>
-            <h2 className={css.countNumber}>
-              {Number(deathCount).toLocaleString()}
-            </h2>
+            <h2 className={css.countNumber}>{Number(deathCount).toLocaleString()}</h2>
           </div>
           <div className={css.card} style={{ color: "#82ca9d" }}>
             <h2 className={css.cardHeader}>Recovered</h2>
-            <h2 className={css.countNumber}>
-              {Number(recoveredCount).toLocaleString()}
-            </h2>
+            <h2 className={css.countNumber}>{Number(recoveredCount).toLocaleString()}</h2>
           </div>
         </section>
 
         <h2 className={css.subtitle} style={{ marginTop: "2rem" }}>
           Country Data
         </h2>
-        <select
-          className={css.input}
-          placeholder="select country"
-          onChange={getCountryData}
-          value={country}
-        >
+        <select className={css.input} placeholder="select country" onChange={getCountryData} value={country}>
           {countries.map(country => (
             <option key={country.name} value={country.name}>
               {country.name}
@@ -126,27 +120,18 @@ const Home = ({ global, countries }) => {
             <div className="lds-dual-ring"></div>
           </div>
         ) : (
-          <section
-            className={`${css.cardContainer} ${css.countryCardContainer}`}
-            style={{ marginTop: "1rem" }}
-          >
+          <section className={`${css.cardContainer} ${css.countryCardContainer}`} style={{ marginTop: "1rem" }}>
             <div className={css.card} style={{ color: "#ffc107" }}>
               <h2 className={css.cardHeader}>Confirmed</h2>
-              <h2 className={css.countNumber}>
-                {Number(countryData.data.confirmed).toLocaleString()}
-              </h2>
+              <h2 className={css.countNumber}>{Number(countryData.data.confirmed).toLocaleString()}</h2>
             </div>
             <div className={css.card} style={{ color: "#dc3545" }}>
               <h2 className={css.cardHeader}>Deaths</h2>
-              <h2 className={css.countNumber}>
-                {Number(countryData.data.deaths).toLocaleString()}
-              </h2>
+              <h2 className={css.countNumber}>{Number(countryData.data.deaths).toLocaleString()}</h2>
             </div>
             <div className={css.card} style={{ color: "#82ca9d" }}>
               <h2 className={css.cardHeader}>Recovered</h2>
-              <h2 className={css.countNumber}>
-                {Number(countryData.data.recovered).toLocaleString()}
-              </h2>
+              <h2 className={css.countNumber}>{Number(countryData.data.recovered).toLocaleString()}</h2>
             </div>
           </section>
         )}
@@ -166,25 +151,18 @@ const Home = ({ global, countries }) => {
             padding: 0,
             width: "100%",
             height: "400px"
-          }}
-        >
+          }}>
           <TimeseriesGraphClient />
         </section>
       </main>
 
       <footer className={css.footer}>
         Data taken from{" "}
-        <a
-          className={css.link}
-          href="https://github.com/CSSEGISandData/COVID-19"
-        >
+        <a className={css.link} href="https://github.com/CSSEGISandData/COVID-19">
           transformating data
         </a>{" "}
         from Johns Hopkins CSSE &{" "}
-        <a
-          className={css.link}
-          href="https://github.com/mathdroid/covid-19-api"
-        >
+        <a className={css.link} href="https://github.com/mathdroid/covid-19-api">
           covid-19-api
         </a>
       </footer>
