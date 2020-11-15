b5a884113693a65d5d6c053430bcad835ceaa6c6
Jibin Thomas <jibin.thomas2706@gmail.com>
2020-11-16 00:23:19 +0530

    Update dependencies

--- pages/api/global.js
+++ pages/api/global.js
@@ -1,18 +1,16 @@
-import fetch from "node-fetch";
-
 export default async (req, res) => {
   let totalGlobalCases = { confirmed: 0, deaths: 0, recovered: 0 };
 
   if (req.method === "GET") {
     await fetch("https://covid19.mathdro.id/api/")
-      .then(response => {
+      .then((response) => {
         return response.json();
       })
-      .then(result => {
+      .then((result) => {
         totalGlobalCases = {
           confirmed: result.confirmed.value,
           deaths: result.deaths.value,
-          recovered: result.recovered.value
+          recovered: result.recovered.value,
         };
       });
 
